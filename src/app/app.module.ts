fddd39567d20244075f985c8e6bbfae59e8a4f5a
analebyodkina <dp030992cad@gmail.com>
2023-04-06 17:17:11 +0300

    created new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,33 @@
 import { NgModule } from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+
+
+// определение маршрутов
+const routes: Routes =[
+  { path: '', component: HomeComponent},
+  { path: 'about', component: AboutComponent},
+  // { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule,
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
